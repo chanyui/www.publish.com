9750df521011181217b98d5150539323f5c4c1f7
yc <718653142@qq.com>
2017-12-22 17:39:49 +0800

    用户唯一

--- app/Admin/Model/UserModel.class.php
+++ app/Admin/Model/UserModel.class.php
@@ -16,17 +16,18 @@ class UserModel extends Model
      * @var array
      */
     protected $_validate = array(
-        array('name','require','用户名不能为空'),
-        array('name','/^([\w\d_]){6,20}$/','用户名长度限制在6-20位，并且只能是英文或数字包括下划线!'),
-        array('password','require','密码不能为空'),
-        array('password','/^([\w\d]){8,16}$/','密码必须是8-16位，并只能是英文和数字',0,'regex',3),
+        array('name', 'require', '用户名不能为空'),
+        array('name', '/^([\w\d_]){6,20}$/', '用户名长度限制在6-20位，并且只能是英文或数字包括下划线!'),
+        array('name', '', '账号已存在！', 0, 'unique', '1'),
+        array('password', 'require', '密码不能为空'),
+        array('password', '/^([\w\d]){8,16}$/', '密码必须是8-16位，并只能是英文和数字', 0, 'regex', 3),
     );
 
     /**
      * @var array
      */
-    protected $_auto =array(
-        array('password','md5',1,'function'),
-        array('update_time','time',2,'function'),
+    protected $_auto = array(
+        array('password', 'md5', 1, 'function'),
+        array('update_time', 'time', 2, 'function'),
     );
 }
